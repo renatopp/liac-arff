45585d9086c042cb54f275579d4094ca9b150210
Lyle Lee <mamadontgodaddycomehome@gmail.com>
2013-11-11 01:36:53 +0800

    The format for the @attribute statement is:
        @attribute <attribute-name> <datatype>
    
    where the <attribute-name> must start with an alphabetic character. If spaces are to be included in the name then the entire name must be quoted.

---
+++ test_arff.py
@@ -0,0 +1,30 @@
+__author__ = 'Hongjoo Lee'
+__author_email__ = 'mamadontgodaddycomehome@gmail.com'
+
+import unittest
+
+import arff
+
+class TestARFF(unittest.TestCase):
+    def setUp(self):
+        arff_text = \
+'''
+@relation test
+
+@attribute number_of_files numeric
+@attribute 'lines of code' numeric
+@attribute defect density numeric
+
+@data 10,10
+'''
+        self.__arff = arff.loads(arff_text)
+
+
+    def test_space_can_be_included_in_the_attribute_name(self):
+        attributes = self.__arff['attributes']
+        self.assertEqual(attributes[0][0], 'number_of_files')
+        self.assertEqual(attributes[1][0], 'lines of code')
+        self.assertEqual(attributes[2][0], 'defect density')
+
+if __name__ == '__main__':
+    unittest.main()
