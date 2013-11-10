e3b7d4d90f948770f160fad721fc9384ae1ae333
Hongjoo Lee <mamadontgodaddycomehome@gmail.com>
2013-11-11 01:43:31 +0800

    Test relation-name having space

--- test_arff.py
+++ test_arff.py
@@ -9,7 +9,7 @@ class TestARFF(unittest.TestCase):
     def setUp(self):
         arff_text = \
 '''
-@relation test
+@relation software metric
 
 @attribute number_of_files numeric
 @attribute 'lines of code' numeric
@@ -25,6 +25,10 @@ class TestARFF(unittest.TestCase):
         self.assertEqual(attributes[0][0], 'number_of_files')
         self.assertEqual(attributes[1][0], 'lines of code')
         self.assertEqual(attributes[2][0], 'defect density')
+
+    def test_space_can_be_included_in_the_relation_name(self):
+        relation = self.__arff['relation']
+        self.assertEqual(relation, 'software metric')
 
 if __name__ == '__main__':
     unittest.main()
