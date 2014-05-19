1cb264d4f77a973a4b69d4bfe3bbce69faee64a2
Piotr Szymański <niedakh@gmail.com>
2014-05-19 18:03:46 +0200

    - implement support for decoding sparse data by unsparsing it - the quickest way, works for me as many .arff loaders can't load sparse files

--- tests/test_decode_data.py
+++ tests/test_decode_data.py
@@ -32,3 +32,22 @@ class TestDecodeData(unittest.TestCase):
         self.assertEqual(result[1], expected[1])
         self.assertEqual(result[2], expected[2])
         self.assertEqual(result[3], expected[3])
+
+    def test_sparse(self):
+        '''Basic data instances.'''
+        decoder = self.get_decoder([
+            ConversorStub(unicode),
+            ConversorStub(float),
+            ConversorStub(int),
+            ConversorStub(unicode),
+            ConversorStub(float),
+            ConversorStub(int),
+        ])
+
+        fixture = u'{0 Iris,1 3.4,2 2}'
+        result = decoder._decode_data(fixture)
+        expected = [u'Iris', 3.4, 2, u'0', 0.0, 0]
+
+        self.assertEqual(len(result), len(expected))
+        for i in range(len(expected)):
+            self.assertEqual(result[i], expected[i])
